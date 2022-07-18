a3f5114b0c677c027b23ab8ca1265f223be21f32
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-07-18 11:07:30 +0530

    Create commandline.java

---
+++ java/commandline.java
@@ -0,0 +1,10 @@
+public class commandline
+{
+public static void main(String args[])
+{
+	System.out.println("The name of student is = "+args[0]);
+	System.out.println("The erno. of student is = "+args[1]);
+	System.out.println("The address of student is = "+args[2]);
+
+}	
+}
