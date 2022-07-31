b684c95c4964f7a6b8c2e90b5f23b78eb51ac38e
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-07-31 11:54:47 +0530

    Create Main method overloading test.java
    We can have any number of main method in a class by method overloading, this because of JVM always calls main() method which receive string array as arguments only

---
+++ java/Main method overloading test.java
@@ -0,0 +1,13 @@
+class test {
+    public static void main(String args[]) {
+        System.out.println("1");
+        test t = new test();
+        t.main(20);
+    }
+
+    public static void main(int a) {
+        System.out.println("2");
+
+    }
+
+}
